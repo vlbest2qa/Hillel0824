2a791ad70aa52f11068811cbc1f2785cf83bae36
Владислав Котелевец <vlbest2@gmail.com>
2024-09-09 12:30:09 +0300

    Add base file

---
+++ SolarTechnology/Pages/BasePage.cs
@@ -0,0 +1,29 @@
+﻿using OpenQA.Selenium;
+
+namespace SolarTechnology.Pages
+{
+    internal class BasePage
+    {
+        public BasePage(IWebDriver driver)
+        {
+            _driver = driver;
+            _js = (IJavaScriptExecutor)_driver;
+        }
+
+        public IJavaScriptExecutor _js;
+        public IWebDriver _driver;
+
+        public void NavigateTo(string link)
+        {
+            _driver.Navigate().GoToUrl(link);
+        }
+
+        public void ScrollTo(IWebElement element)
+        {
+            _js.ExecuteScript("arguments[0].scrollIntoView(true);", element);
+            //_js.ExecuteScript("arguments[0].scrollIntoView(false);", element);
+        }
+
+
+    }
+}
---
+++ SolarTechnology/Tests/UITestFixture.cs
@@ -0,0 +1,28 @@
+﻿using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+
+
+namespace SolarTechnology.Tests
+{
+    public class BaseClass
+    {
+        public IWebDriver _driver;
+        public IJavaScriptExecutor _js;
+
+        [SetUp]
+        public void Setup()
+        {
+            var options = new ChromeOptions();
+            options.AddArgument("window-size=1400,1200");
+            _driver = new ChromeDriver(options);
+            _driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(0);
+            _js = (IJavaScriptExecutor)_driver;
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _driver.Quit();
+        }
+    }
+}
