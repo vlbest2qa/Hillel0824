7489317bd338a1151fccf3e346e49b62aeef3efd
Владислав Котелевец <vlbest2@gmail.com>
2024-09-13 15:43:21 +0300

    add 1rst test

---
+++ Selenium/Selenium/Pages/WebTablesPage.cs
@@ -0,0 +1,55 @@
+﻿using OpenQA.Selenium;
+
+namespace Selenium
+{
+    internal class WebTablesPage
+    {
+        public WebTablesPage(IWebDriver driver)
+        {
+            _driver = driver;
+        }
+        public IWebDriver _driver;
+
+        private string pageUrl = "https://demoqa.com/webtables";
+        private By addButtonBy = By.Id("addNewRecordButton");
+        private By firstNameBy = By.Id("firstName");
+        private By lastNameBy = By.Id("lastName");
+        private By emailBy = By.Id("userEmail");
+        private By ageBy = By.Id("age");
+        private By salaryBy = By.Id("salary");
+        private By departmentBy = By.Id("department");
+        private By SubmitButtonBy = By.Id("submit");
+        
+
+        public void Open()
+        {
+            _driver.NavigateTo(pageUrl);
+        }
+
+        public void ClickAddButton()
+        {
+            _driver.ClickElement(addButtonBy);
+        }
+
+        public void FillRegistrationForm(string firstName, string lastName, string email, string age, string salary, string department)
+        {
+            _driver.FillInput(firstNameBy, firstName);
+            _driver.FillInput(lastNameBy, lastName);
+            _driver.FillInput(emailBy, email);
+            _driver.FillInput(ageBy, age);
+            _driver.FillInput(salaryBy, salary);
+            _driver.FillInput(departmentBy, department);
+        }
+
+        public void ClickFormSubmitButton()
+        {
+            _driver.ClickElement(SubmitButtonBy);
+        }
+
+        public bool СheckAddedSting(string value, string whichString)
+        {
+            By ResultStringBy = By.XPath($"//*[@class='rt-tbody']//*[@class='rt-tr-group']//*[contains(@class,'rt-tr') and *[@class='rt-td' and text()='{whichString}']]");
+            return _driver.GetTextElement(ResultStringBy).Contains(value);
+        }
+    }
+}
---
+++ Selenium/Selenium/Tests/WebTablesTests.cs
@@ -0,0 +1,52 @@
+﻿namespace Selenium
+{
+    internal class WebTablesTests : BaseClass
+    {
+        [Test]
+        public void AddAndVerifyStringInTable()
+        {
+            // Arrange
+            var webTablesPage = new WebTablesPage(_driver);
+            string firstName = "Vladyslav";
+            string lastName = "Kotelevets";
+            string age = "30";
+            string email = "mymail@example.com";
+            string salary = "100";
+            string department = "Engineering";
+
+            webTablesPage.Open();
+
+            // Act
+            webTablesPage.ClickAddButton();
+            webTablesPage.FillRegistrationForm(firstName, lastName, email, age, salary, department);
+            webTablesPage.ClickFormSubmitButton();
+
+            // Assert
+            //Зробив перевірки що доданна строка містить усі елементи які додавали. А як оріентир для цієї строки використав firstName.
+            Assert.Multiple(() =>
+            {
+                Assert.That(webTablesPage.СheckAddedSting(firstName, firstName), Is.EqualTo(true), "First Name not added or outside the result string");
+                Assert.That(webTablesPage.СheckAddedSting(lastName, firstName), Is.EqualTo(true), "Last Name not added or outside the result string");
+                Assert.That(webTablesPage.СheckAddedSting(age, firstName), Is.EqualTo(true), "Age not added or outside the result string");
+                Assert.That(webTablesPage.СheckAddedSting(email, firstName), Is.EqualTo(true), "Email not added or outside the result string");
+                Assert.That(webTablesPage.СheckAddedSting(salary, firstName), Is.EqualTo(true), "Salary not added or outside the result string");
+                Assert.That(webTablesPage.СheckAddedSting(department, firstName), Is.EqualTo(true), "Departmant not added or outside the result string");
+            });
+        }
+
+        [TestCase("Cierra")]
+        [TestCase("Alden")]
+        [TestCase("Kierra")]
+
+        public async Task RemoveRecordFromTable(string firstName)
+        {
+            // Arrange
+            var webTablesPage = new WebTablesPage(_driver);
+            webTablesPage.Open();
+
+            // Act
+
+            // Assert
+        }
+    }
+}
