0b0c296a95bd47161c58c61a0c6e5f91858c34a6
Владислав Котелевец <vlbest2@gmail.com>
2024-09-15 21:59:41 +0300

    HW16

--- AtataDemoQA/AtataDemoQA/TextBoxPage.cs
+++ AtataDemoQA/AtataDemoQA/TextBoxPage.cs
@@ -3,7 +3,34 @@ using _ = AtataDemoQA.TextBoxPage;
 
 namespace AtataDemoQA
 {
+    [Url("/text-box")]
     public sealed class TextBoxPage : Page<_>
     {
+        [FindById("userName")]
+        public TextInput<_> FullName { get; set; }
+
+        [FindById("userEmail")]
+        public EmailInput<_> Email { get; set; }
+
+        [FindByXPath("//*[@placeholder='Current Address']")]
+        public TextArea<_> CurrentAddres { get; set; }
+
+        [FindByXPath("//*[@class='col-md-9 col-sm-12']//*[@id='permanentAddress']")]
+        public TextArea<_> PermanentAddress { get; set; }
+
+        [FindById("submit")]
+        public Button<_> Submit { get; set; }
+
+        [FindById("name")]
+        public Text<_> FullNameResult { get; set; }
+
+        [FindById("email")]
+        public Text<_> EmailResult { get; set; }
+
+        [FindByCss("#output #currentAddress.mb-1")]
+        public Text<_> CurrentAddressResult { get; set; }
+
+        [FindByXPath("//*[@Id='output']//*[@id='permanentAddress']")]
+        public Text<_> PermanentAddressResult { get; set; }
     }
 }
--- AtataDemoQA/AtataDemoQA/TextBoxTests.cs
+++ AtataDemoQA/AtataDemoQA/TextBoxTests.cs
@@ -1,5 +1,6 @@
 ﻿using Atata;
 using NUnit.Framework;
+using OpenQA.Selenium.DevTools.V125.DOM;
 using System;
 
 namespace AtataDemoQA
@@ -7,8 +8,26 @@ namespace AtataDemoQA
     public sealed class TextBoxTests : UITestFixture
     {
         [Test]
-        public void Test()
+        public void FillAndSubmitTextBox()
         {
+            string fullName = "Vladyslav";
+            string email = "myemail@gmail.com";
+            string currentAdress = "4B Serpova street, Kharkiv, Ukraine";
+            string permanentAdress = "8 Berejnia street, Vasisheve, Ukraine";
+
+            Go.To<TextBoxPage>()
+                .FullName.Set(fullName)
+                .Email.Set(email)
+                .CurrentAddres.Set(currentAdress)
+                .PermanentAddress.ScrollTo().ScrollDown()
+                .PermanentAddress.Set(permanentAdress)
+                .Submit.ScrollTo().ScrollDown()
+                .Submit.Click()
+                .FullNameResult.ScrollTo().ScrollDown()
+                .FullNameResult.Should.Be("Name:" + fullName)
+                .EmailResult.Should.Be("Email:" + email)
+                .CurrentAddressResult.Should.Be("Current Address :" + currentAdress)
+                .PermanentAddressResult.Should.Be("Permananet Address :" + permanentAdress);
         }
     }
 }
