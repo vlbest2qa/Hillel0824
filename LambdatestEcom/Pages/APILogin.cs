cae756fd9b4a6779f0e3bb4abf4ba198ff575848
Владислав Котелевец <vlbest2@gmail.com>
2024-10-08 23:49:38 +0300

    HW25

---
+++ LambdatestEcom/Pages/APILogin.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Playwright;
+
+namespace LambdatestEcom.Pages
+{
+    internal class APILogin
+    {
+        private readonly IPage _page;
+
+        public APILogin(IPage page)
+        {
+            _page = page;
+        }
+
+        public async Task<bool> APIlogin(string email, string password)
+        {
+            var multipart = _page.APIRequest.CreateFormData();
+            multipart.Append("email", email);
+            multipart.Append("password", password);
+
+            var response = await _page.APIRequest
+                .PostAsync("https://ecommerce-playground.lambdatest.io/index.php?route=account/login", 
+                new() { Form = multipart });
+            /*
+             * Хотів перевірити респонс якщо не вийшло авторизуватись і якось обробити це, але він все одно шле 200=)
+             * Тож залишив else як є, бо не знаю як там буде.
+             * Можливо Task<bool> змінити на string і в ньому описувати результати і порівнювати з очікуємим.
+             */
+            if (response.Ok) 
+            {
+                return response.Ok;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ LambdatestEcom/Tests/APILoginTest.cs
@@ -0,0 +1,28 @@
+﻿using LambdatestEcom.Pages;
+
+namespace LambdatestEcom.Tests
+{
+    [Parallelizable(ParallelScope.Self)]
+    [TestFixture]
+    public class APILoginTest : UITestFixture
+    {
+        [Test]
+        public async Task LoginAPI()
+        {
+            // Arrange
+            var apiLogin = new APILogin(page);
+            var userCabinet = new UserCabinetPage(page);
+
+            string email = "mymail@gmail.com";
+            string password = "test";
+
+            // Act
+            Assert.That(await apiLogin.APIlogin(email, password), Is.True); 
+            await userCabinet.Open();
+
+            // Assert
+            StringAssert.EndsWith("?route=account/account", page.Url);
+            Assert.That(userCabinet.IsUserCabinetOpen().Result, Is.True);
+        }
+    }
+}
