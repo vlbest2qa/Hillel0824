879118fdd58a6aa0911aa909b70bd4f8bb83999a
Владислав Котелевец <vlbest2@gmail.com>
2024-10-08 19:09:24 +0300

    HW23

---
+++ LambdatestEcom/Pages/EditCartPage.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Playwright;
+
+namespace LambdatestEcom.Pages
+{
+    internal class EditCartPage
+    {
+        private readonly IPage _page;
+
+        public EditCartPage(IPage page)
+        {
+            _page = page;
+        }
+
+        public async Task<string> GetProductQuantity(string productName)
+        {
+            //рабочий общий:
+            //return await _page.Locator(".table").Filter(new() { HasText = productName }).Locator("input[type='text']").InputValueAsync();
+            return await _page.Locator($"tr:has-text('{productName}') input[name^='quantity']").InputValueAsync();
+        }
+
+        public async Task<string> GetCartTotal()
+        {
+            return await _page.Locator("#content").GetByRole(AriaRole.Row, new() { Name = "Total:" }).Nth(1).TextContentAsync();
+        }
+
+        public async Task ClickContinueShopping()
+        {
+            await _page.GetByRole(AriaRole.Link, new() { Name = "Continue Shopping" }).ClickAsync();
+        }
+
+        public async Task ChangeProductQuantityAndConfirm(string productName, string newQuantity)
+        {
+            await _page.Locator($"tr:has-text('{productName}') input[name^='quantity']").FillAsync(newQuantity);
+            await _page.Locator($"tr:has-text('{productName}') button[type^='submit']").ClickAsync();
+        }
+    }
+}
---
+++ LambdatestEcom/Pages/ProductPage.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Playwright;
+
+namespace LambdatestEcom.Pages
+{
+    internal class ProductPage
+    {
+        private readonly IPage _page;
+
+        public ProductPage(IPage page)
+        {
+            _page = page;
+        }
+
+        public async Task SetItemQuantity(string quantity)
+        {
+            await _page.GetByRole(AriaRole.Spinbutton, new() { Name = "Qty" }).FillAsync(quantity);
+        }
+
+        public async Task AddProductToCart()
+        {
+            await _page.GetByRole(AriaRole.Button, new() { Name = "Add to Cart" }).ClickAsync();
+        }
+    }
+}
