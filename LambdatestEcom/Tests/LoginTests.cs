9ebff8c54fe8e19dde3c5abb1845f2ffa7dc74c0
Владислав Котелевец <vlbest2@gmail.com>
2024-10-17 18:39:29 +0300

    Option 1 like a lasson

---
+++ LambdatestEcom/Tests/LoginTests.cs
@@ -0,0 +1,23 @@
+﻿using LambdatestEcom.Pages;
+using Microsoft.Playwright;
+
+namespace LambdatestEcom.Tests
+{
+    [Parallelizable(ParallelScope.Self)]
+    [TestFixture]
+    public class LoginTests : UITestFixture
+    {
+        [Test]
+        public async Task StateWithAuthIsLoaded()
+        {
+            // Arrange
+            var userCabinet = new UserCabinetPage(page);
+
+            // Act
+            await userCabinet.Open();
+
+            // Assert
+            await Assertions.Expect(userCabinet.PageHeader()).ToBeVisibleAsync();
+        }
+    }
+}
--- LambdatestEcom/UITestFixture.cs
+++ LambdatestEcom/UITestFixture.cs
@@ -19,13 +19,23 @@ namespace LambdatestEcom
                 Headless = ciEnv == "true"
             });
 
+            // File, Path to save session
+            string subPath = "../../../playwright/.auth";
+            string filePath = "../../../playwright/.auth/state.json";
+            if (!Directory.Exists(subPath))
+                Directory.CreateDirectory(subPath);
+            if (!File.Exists(filePath))
+                File.AppendAllText(filePath, "{}");
+
             context = await browser.NewContextAsync(new BrowserNewContextOptions
             {
                 ViewportSize = new ViewportSize
                 {
                     Width = 1920,
                     Height = 1080
-                }
+                },
+                IgnoreHTTPSErrors = true,
+                StorageStatePath = filePath
             });
 
             await context.Tracing.StartAsync(new()
@@ -37,6 +47,18 @@ namespace LambdatestEcom
             });
 
             page = await context.NewPageAsync();
+
+            await page.GotoAsync("https://ecommerce-playground.lambdatest.io/index.php?route=account/account");
+            if (!await page.GetByRole(AriaRole.Heading, new() { Name = "My Account" }).IsVisibleAsync())
+            {
+                await page.GetByPlaceholder("E-Mail Address").FillAsync("mymail@gmail.com");
+                await page.GetByPlaceholder("Password").FillAsync("test");
+                await page.GetByRole(AriaRole.Button, new() { Name = "Login" }).ClickAsync();
+                await context.StorageStateAsync(new()
+                {
+                    Path = filePath
+                });
+            }
             //page.SetDefaultTimeout(5000);
         }
 
