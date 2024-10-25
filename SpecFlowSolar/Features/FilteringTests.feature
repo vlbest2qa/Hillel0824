2a80eaac84b5d137fed806ec261098ca73e963a1
Владислав Котелевец <vlbest2@gmail.com>
2024-10-25 18:16:22 +0300

    First

--- SpecFlowSolar/Features/FilteringTests.feature
+++ SpecFlowSolar/Features/FilteringTests.feature
@@ -1,7 +1,17 @@
 ﻿Feature: FilteringTests
 
 @filteringTests
-Scenario: verify the product count after filtering
+Scenario: verify the product count after filtering 1
+	Given open home page
+	When open solar cabels in menu
+	And count product before filtered
+	And open filters
+	And choose brand 'JA Solar' in filters
+	And count product after filtered
+	Then the quantity of products is different
+
+@filteringTests
+Scenario: verify the product count after filtering 2
 	Given open home page
 	When open solar cabels in menu
 	And count product before filtered
--- SpecFlowSolar/StepDefinitions/CartTestsStepDefinitions.cs
+++ SpecFlowSolar/StepDefinitions/CartTestsStepDefinitions.cs
@@ -7,53 +7,50 @@ namespace SpecFlowSolar.StepDefinitions
     [Scope(Tag = "cartTests")]
     public class CartTestsStepDefinitions : BaseClass
     {
+        public CartTestsStepDefinitions(ScenarioContext scenarioContext) : base(scenarioContext)
+        {
+        }
+
         [Given(@"open home page")]
 
         public void GivenOpenHomePage()
         {
-            var homePage = new HomePage(_driver);
             homePage.OpenHomePage();
         }
 
         [When(@"open solar cabels in menu")]
         public void WhenOpenSolarCabelsInMenu()
         {
-            var homePage = new HomePage(_driver);
             homePage.OpenSolarCabels();
         }
 
         [When(@"add second product to cart")]
         public void WhenAddSecondProductToCart()
         {
-            var catalogPage = new CatalogPage(_driver);
             catalogPage.AddToCartOneProduct(1);
         }
 
         [When(@"go to cart in popup window")]
         public void WhenGoToCartInPopupWindow()
         {
-            var catalogPage = new CatalogPage(_driver);
             catalogPage.GoToCartInModal();
         }
 
         [When(@"remove product from cart")]
         public void WhenRemoveProductFromCart()
         {
-            var cartPage = new CartPage(_driver);
             cartPage.RemoveProductFromCartIfOne();
         }
 
         [Then(@"current url is equal to home url")]
         public void ThenCurrentUrlIsEqualToHomeUrl()
         {
-            var homePage = new HomePage(_driver);
             Assert.That(homePage.IsCurentUrlEqualHome(), Is.EqualTo(true), "Current url NOT equal home url.");
         }
 
         [Then(@"title is displayed")]
         public void ThenTitleIsDisplayed()
         {
-            var homePage = new HomePage(_driver);
             Assert.That(homePage.IsTitleDisplayed(), Is.EqualTo(true), "Not found title on the page.");
         }
 
