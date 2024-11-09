c649c722cca4c29467d4ae800436da964778f149
Владислав Котелевец <vlbest2@gmail.com>
2024-11-09 02:52:37 +0200

    HW34

--- Evershop.Tests/Evershop.Tests.API/Tests/ApiTests.cs
+++ Evershop.Tests/Evershop.Tests.API/Tests/ApiTests.cs
@@ -10,7 +10,6 @@ namespace Evershop.Tests.API.Tests
     {
         private string _sid;
         private CookieCollection _cookies;
-        private string _uuid;
 
         [SetUp]
         public async Task SetupAsync()
@@ -27,7 +26,7 @@ namespace Evershop.Tests.API.Tests
         }
 
         [Test]
-        public async Task CreateProduct()
+        public async Task CreateAndDeleteProduct()
         {
             // Arrange
             var request = new RestRequest("api/products", Method.Post);
@@ -68,32 +67,85 @@ namespace Evershop.Tests.API.Tests
 
             // Act
             var response = await App.ApiClient.PostAsync<ProductResponseData>(request);
-            _uuid = response.Data.Data.Uuid;
+            var _uuid = response.Data.Data.Uuid;
 
             // Assert
             response.AssertStatusCode(HttpStatusCode.OK);
             Assert.IsNotNull(response.Data.Data);
             Assert.IsNotNull(response.Data.Data.ProductDescriptionId);
             response.AssertExecutionTimeUnder(1);
+
+            //Delete product after create
+
+            // Arrange
+            var deleteRequest = new RestRequest($"api/products/{_uuid}", Method.Delete);
+            for (int i = 0; i < _cookies.Count; i++)
+            {
+                deleteRequest.AddCookie(_cookies[i].Name, _cookies[i].Value, _cookies[i].Path, _cookies[i].Domain);
+            }
+            deleteRequest.AddHeader("Authorization", $"Bearer {_sid}");
+
+            // Act
+            var deleteResponse = await App.ApiClient.DeleteAsync(deleteRequest);
+
+            // Assert
+            deleteResponse.AssertStatusCode(HttpStatusCode.OK);
         }
 
-        [TearDown]
-        public async Task TearDownAsync()
+        [Test]
+        public async Task CreateAndDeleteCategory()
         {
-            if (_uuid != null)
+            // Arrange
+            var requestAdd = new RestRequest("api/categories", Method.Post);
+
+            for (int i = 0; i < _cookies.Count; i++)
             {
-                // Delete
-                var request = new RestRequest($"api/products/{_uuid}", Method.Delete);
-                for (int i = 0; i < _cookies.Count; i++)
-                {
-                    request.AddCookie(_cookies[i].Name, _cookies[i].Value, _cookies[i].Path, _cookies[i].Domain);
-                }
-                request.AddHeader("Authorization", $"Bearer {_sid}");
-                var deleteResponse = await App.ApiClient.DeleteAsync(request);
-
-                deleteResponse.AssertStatusCode(HttpStatusCode.OK);
+                requestAdd.AddCookie(_cookies[i].Name, _cookies[i].Value, _cookies[i].Path, _cookies[i].Domain);
             }
+            requestAdd.AddHeader("Authorization", $"Bearer {_sid}");
 
+            var category = new Category()
+            {
+                Name = "New Category",
+                ParentId = "",
+                CategoryId = "",
+                Description = "[]",
+                UrlKey = "new_category",
+                MetaTitle = "Title Category",
+                MetaKeywords = "Category Keywords",
+                MetaDescription = "Category Description",
+                Image = "",
+                Status = "1",
+                IncludeInNav = "1",
+                ShowProducts = "1",
+            };
+            requestAdd.AddJsonBody(JsonConvert.SerializeObject(category));
+
+            // Act
+            var responseAdd = await App.ApiClient.PostAsync<CategoryResponseData>(requestAdd);
+            var _uuid = responseAdd.Data.Data.Uuid;
+
+            // Assert
+            responseAdd.AssertStatusCode(HttpStatusCode.OK);
+            Assert.That(responseAdd.Data.Data, Is.Not.Null);
+            Assert.That(responseAdd.Data.Data.Name, Is.EqualTo(category.Name));
+            responseAdd.AssertExecutionTimeUnder(1);
+
+            //Delete category after create
+
+            // Arrange
+            var requestDelete = new RestRequest($"/api/categories/{_uuid}", Method.Delete);
+            for (int i = 0; i < _cookies.Count; i++)
+            {
+                requestDelete.AddCookie(_cookies[i].Name, _cookies[i].Value, _cookies[i].Path, _cookies[i].Domain);
+            }
+            requestDelete.AddHeader("Authorization", $"Bearer {_sid}");
+
+            // Act
+            var responseDelete = await App.ApiClient.DeleteAsync(requestDelete);
+
+            // Assert
+            responseDelete.AssertStatusCode(HttpStatusCode.OK);
         }
     }
 
